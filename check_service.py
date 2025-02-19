bfbc6e3be624db4fba6f6c81e9ff55df5196de72
“hauchenglee” <hauchenglee@gmail.com>
2025-02-19 12:03:29 +0800

    style(service): add comment

--- check_service.py
+++ check_service.py
@@ -1,5 +1,27 @@
+# 使用 SpellChecker + re + pycorrector 作为基本的中文与英文错别字文字检查
+# 进阶功能，加上List, Dict + re作为上下文语义检查
+
 class CheckService:
     def process_data(self, data):
+        article = data.get('article', '')
+        terms = data.get('terms', [])
+        is_context = data.get('is_context', False)
+
+        # {
+        #    "article":"身心障礙者生活補助",
+        #    "terms":[
+        #       "低收入戶",
+        #       "中低收入戶",
+        #       "身心障礙證明",
+        #       "身心障礙者生活補助",
+        #       "育兒津貼",
+        #       "托育補助",
+        #       "人籍合一",
+        #       "設籍並居住"
+        #    ],
+        #    "is_context":false
+        # }
+
         # 这里放你的业务逻辑
         result = {
             "message": "Processed by CheckService",
